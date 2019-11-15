d0b905258702399ac93d16a8a47c5f6448f19fbd
Amadeusz <amadeusz.filipek@gmail.com>
2019-11-15 18:07:42 +0100

    Initial commit

---
+++ boardgame_timer/settings.py
@@ -0,0 +1,43 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SECRET_KEY = 'nu_0!kcvi8^una38f#w2b9t5!6ktf*t4!$vygw@kc5e*x960h^'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+ROOT_URLCONF = 'boardgame_timer.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ['boardgame_timer'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+WSGI_APPLICATION = 'boardgame_timer.wsgi.application'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'boardgame_timer/static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
