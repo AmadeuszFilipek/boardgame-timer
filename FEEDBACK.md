1280dbe434b8387fb83a5d7e2aefc3066ac9783a
Amadeusz Filipek <amadeusz.filipek@gmail.com>
2026-06-21 18:51:23 +0200

    cleanup old stuff

---
+++ FEEDBACK.md
@@ -0,0 +1,5 @@
+
+
+# Countdown timer
+
+- end of time behavior - remove the "GAME OVER" window - this is not required in any spec
--- boardgame_timer/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from . import consumers # Will be created next
-
-websocket_urlpatterns = [
-    re_path(r'ws/session/(?P<session_slug>[^/]+)/$', consumers.SessionConsumer.as_asgi()),
-]
